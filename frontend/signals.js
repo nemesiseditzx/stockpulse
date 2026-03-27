492aa28d9344af3445855e309983de299aedabd4
nemesiseditzx <nemesiseditzx984@gmail.com>
2026-03-26 22:44:11 -0400

    Update signals.js

--- frontend/signals.js
+++ frontend/signals.js
@@ -1,33 +1,29 @@
-const API="https://stockpulsebadhoneditzx.up.railway.app";
+const API = "https://stockpulsebadhoneditzx.up.railway.app";
 
 function loadSignals(){
 
-  // 🔥 CURRENT
+  // CURRENT
   fetch(API + "/signals-current")
   .then(res => res.json())
   .then(data => {
-
     const feed = document.getElementById("current");
-    feed.innerHTML="";
+    feed.innerHTML = "";
 
     data.forEach(msg => {
       feed.appendChild(createMsg(msg));
     });
-
   });
 
-  // 🕓 PREVIOUS
+  // PREVIOUS
   fetch(API + "/signals-previous")
   .then(res => res.json())
   .then(data => {
-
     const feed = document.getElementById("previous");
-    feed.innerHTML="";
+    feed.innerHTML = "";
 
     data.forEach(msg => {
       feed.appendChild(createMsg(msg, true));
     });
-
   });
 
 }
@@ -35,7 +31,7 @@ function loadSignals(){
 function createMsg(msg, old=false){
 
   const div = document.createElement("div");
-  div.className="msg";
+  div.className = "msg";
 
   const time = new Date(msg.time * 1000).toLocaleString();
 
@@ -53,9 +49,9 @@ function format(text){
   text = text.replace(/BUY/gi, "<span style='color:#22c55e'>BUY</span>");
   text = text.replace(/SELL/gi, "<span style='color:#ef4444'>SELL</span>");
 
-  return text;
+  return text.replace(/\n/g, "<br>");
 }
 
-// 🔁 auto refresh
+// AUTO REFRESH
 loadSignals();
 setInterval(loadSignals, 5000);
