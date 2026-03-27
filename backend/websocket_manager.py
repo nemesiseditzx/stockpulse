3ce73c7ba49fd28073308de9532bd05b8722c32e
nemesiseditzx <nemesiseditzx984@gmail.com>
2026-03-26 21:56:41 -0400

    Update websocket_manager.py

--- backend/websocket_manager.py
+++ backend/websocket_manager.py
@@ -1,1 +1,14 @@
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = []
 
+    async def connect(self, ws):
+        await ws.accept()
+        self.active_connections.append(ws)
+
+    def disconnect(self, ws):
+        self.active_connections.remove(ws)
+
+    async def broadcast(self, data):
+        for conn in self.active_connections:
+            await conn.send_json(data)
