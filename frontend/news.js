ef53ba80bd41081af71f1cf9bba674655adcc5ca
nemesiseditzx <nemesiseditzx984@gmail.com>
2026-03-27 17:52:40 -0400

    Update news.js

--- frontend/news.js
+++ frontend/news.js
@@ -10,19 +10,36 @@ function loadNews(){
 
     data.forEach(n => {
 
+      let sentimentColor = "#94a3b8";
+
+      if(n.sentiment === "Bullish") sentimentColor = "#22c55e";
+      if(n.sentiment === "Bearish") sentimentColor = "#ef4444";
+
       container.innerHTML += `
         <div class="news-card" onclick="window.open('${n.url}')">
+
           <img src="${n.image || 'https://picsum.photos/400'}" class="news-img">
 
           <div class="news-content">
+
             <div class="news-title">${n.title}</div>
-            <div class="news-summary">${(n.summary||"").substring(0,100)}...</div>
-            <div style="font-size:12px;margin-top:8px;">
+
+            <div class="news-summary">
+              ${(n.summary||"").substring(0,120)}...
+            </div>
+
+            <div style="margin-top:10px;font-size:13px;">
               ⚡ ${n.effect}
             </div>
-            <div style="font-size:10px;color:gray;margin-top:6px;">
-              Powered by Badhon EditZX
+
+            <div style="margin-top:5px;font-size:13px;">
+              📊 Sector: ${n.sector}
             </div>
+
+            <div style="margin-top:5px;font-size:13px;color:${sentimentColor};">
+              🧠 ${n.sentiment}
+            </div>
+
           </div>
         </div>
       `;
